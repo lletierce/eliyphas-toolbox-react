af334307c2749f17f2f747e13e270c2791c4d1d0
letierce.lo <letierce.lo@gmail.com>
2025-10-06 17:58:03 +0200

    🧑‍💻 util sortByName() for rituals listing

--- src/components/pages/rpg/dnd4/rituals/RitualsPage.js
+++ src/components/pages/rpg/dnd4/rituals/RitualsPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { dnd4Rituals } from "../../../../../data/static-dnd4-rituals-data";
+import { sortByName  } from "../../../../../utils/array"
 import { Link } from "react-router-dom";
 
 export default function RitualsPage() {
+
+const sortedRituals = sortByName(dnd4Rituals.FEYN);
+
   return (
     <div>
       <h1>Feyn rituals</h1>
       <ul>
-        {dnd4Rituals.FEYN.map((ritual) => {
+        {sortedRituals.map((ritual) => {
           return (
             <li key={ritual.id}>
               <Link to={`/ritual/${ritual.id}`}>{ritual.name}</Link>
@@ -17,4 +21,4 @@ export default function RitualsPage() {
       </ul>
     </div>
   );
-}
+} 
--- src/utils/array.js
+++ src/utils/array.js
@@ -1,3 +1,9 @@
 export const findObjectById = (id, array) => {
     return array.find((itemInArray) => itemInArray.id === id)
   }
+
+export function sortByName(array) {
+  return [...array].sort((a, b) => {
+    return a.name.localeCompare(b.name, 'fr', { sensitivity: 'base' });
+  });
+}
